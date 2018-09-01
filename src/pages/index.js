5f67146e6e0ca88b708afc57354dd6f3e5e6edbd
Julius Alvarado <javascript.uiux@gmail.com>
2018-08-31 17:59:15 -0700

    added an iframe of my app

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,20 @@ const IndexPage = () => (
   <div>
     <h1>Hi, I'm Julius Alvarado ^_^/</h1>
     
-    <p>I use AngularJS 1.x, PHP 7, and SQL to do <i>Data Engineering :)</i></p>
+    <p>I use AngularJS 1.x, PHP 7, and SQL to do <i>Full Stack Engineering :)</i></p>
 
     <p><small>I'm also trying to learn Google BigQuery, ai/ml, React and improve my Node.js skills (:</small></p>
-
-<h1><a href="https://github.com/ideaguy3d" target="_blank">github.com/ideaguy3d</a> </h1>
+      
+    <h1><a href="https://github.com/ideaguy3d" target="_blank">github.com/ideaguy3d</a> </h1>
     
-
     <br></br>
 
     <img src="http://hack2016.julius3d.com/img/logo/j3d.png" alt="my-logo" height="54" width="198"></img>
+
+    <br></br>
+
+      <p>App I am working on:</p>
+      <iframe src="http://edhub.co" width="375" height="667"></iframe>
 
     <br></br>
 
